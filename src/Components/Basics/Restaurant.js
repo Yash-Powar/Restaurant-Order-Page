7c3499bad9b3408d553a5f53936607ed9f241a7f
Yash Powar <123502765+Yash-Powar@users.noreply.github.com>
2023-02-20 03:17:35 +0530

    final commit

---
+++ src/Components/Basics/Restaurant.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import './style.css'
+import Menu from './menuApi'
+import MenuCard from './MenuCard'
+import Navbar from './NavBar'
+
+const uniqueList = [...new Set(Menu.map((curElem) => {
+    return curElem.category;
+})
+)];
+
+console.log(uniqueList);
+
+const Restaurant = () => {
+
+    const [menuData, setMenuData] = useState(Menu);
+    const [menuList,setMenuList] = useState(uniqueList); 
+
+    const filterItem = (category) => {
+        const updatedList = Menu.filter((curElem) => {
+            return curElem.category === category;
+        });
+        setMenuData(updatedList);
+    };
+    return (
+        <>
+           <Navbar filterItem={filterItem}  menuList={menuList}/>
+            <MenuCard menuData={menuData} />
+        </>
+    )
+}
+
+export default Restaurant
